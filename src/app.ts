244faf6443e618b2c907670dcc13ae6634590f64
Luana Vefago dos Santos <101883667+luanavfg@users.noreply.github.com>
2024-01-30 13:24:16 -0300

    update: update schema

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.organization.create({
+  data: {
+    name: 'Organization Test',
+    email: 'organization@example.com',
+    address: 'fake address',
+    whatsApp_number: '123-456-789',
+  },
+})
