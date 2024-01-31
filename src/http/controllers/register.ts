5defd9549ace66accb9f48c911696887c42fc579
luanavfg <luanavefago1@gmail.com>
2024-01-31 09:49:41 -0300

    adjust: error treatment globally

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -31,7 +31,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (err instanceof OrganizationAlreadyExistsError) {
       return reply.status(409).send({ message: err.message })
     }
-    return reply.status(500).send()
+    throw err
   }
   return reply.status(201).send()
 }
