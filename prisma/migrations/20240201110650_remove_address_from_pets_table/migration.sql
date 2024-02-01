2d8e15f2ed3696dd3df56ea15e7ead11af47b022
luanavfg <luanavefago1@gmail.com>
2024-02-01 08:28:24 -0300

    update: migrations

---
+++ prisma/migrations/20240201110650_remove_address_from_pets_table/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `address` on the `pets` table. All the data in the column will be lost.
+  - You are about to drop the column `city` on the `pets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "pets" DROP COLUMN "address",
+DROP COLUMN "city";
