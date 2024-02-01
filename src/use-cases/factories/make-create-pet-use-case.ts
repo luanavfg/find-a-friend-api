9018035cb52b16b734ac246f2a12031d4ca94be7
luanavfg <luanavefago1@gmail.com>
2024-02-01 14:10:34 -0300

    feat: add create pet factory

---
+++ src/use-cases/factories/make-create-pet-use-case.ts
@@ -0,0 +1,12 @@
+import { PrismaOrganizationsRepository } from '@/repositories/prisma/prisma-organizations-repository'
+import { PrismaPetsRepository } from '@/repositories/prisma/prisma-pets-repository'
+import { CreatePetUseCase } from '../createPet'
+
+export function makeCreatePetUseCase() {
+  const organizationsRepository = new PrismaOrganizationsRepository()
+  const petsRepository = new PrismaPetsRepository()
+
+  const useCase = new CreatePetUseCase(petsRepository, organizationsRepository)
+
+  return useCase
+}
