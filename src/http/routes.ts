a35e610cac1b559b7dc32145634312818c0dad93
luanavfg <luanavefago1@gmail.com>
2024-02-05 11:25:42 -0300

    feat: add pets route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/organizations/register'
 import { authenticate } from './controllers/organizations/authenticate'
+import { createPet } from './controllers/pets/create-pet'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/organizations', register)
-  // app.post('/pets', createPet)
+  app.post('/pets', createPet)
   app.post('/sessions', authenticate)
 }
