399467317f58ad43692247e4b9b822eeee185d30
luanavfg <luanavefago1@gmail.com>
2024-02-05 11:08:02 -0300

    feat: add sessions route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/organizations/register'
-import { createPet } from './controllers/pets/create-pet'
+import { authenticate } from './controllers/organizations/authenticate'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/organizations', register)
-  app.post('/pets', createPet)
+  // app.post('/pets', createPet)
+  app.post('/sessions', authenticate)
 }
