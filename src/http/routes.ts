cb556e31f3296ac698df5b43d00dc410ee9903b3
luanavfg <luanavefago1@gmail.com>
2024-02-06 08:20:59 -0300

    feat: add middleware in pets route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/organizations/register'
 import { authenticate } from './controllers/organizations/authenticate'
 import { createPet } from './controllers/pets/create-pet'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/organizations', register)
-  app.post('/pets', createPet)
   app.post('/sessions', authenticate)
+
+  app.post('/pets', { onRequest: [verifyJWT] }, createPet)
 }
