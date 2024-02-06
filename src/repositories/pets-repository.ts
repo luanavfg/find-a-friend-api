1756e3ddeb52f04d1e8dbf49a3cc8560830d13a6
luanavfg <luanavefago1@gmail.com>
2024-02-06 08:22:07 -0300

    feat: add findByCity method in pets repository

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,6 +1,12 @@
-import { Prisma, Pet } from '@prisma/client'
+import { Prisma, Pet, Age, Size } from '@prisma/client'
 
+export interface SearchPetsFilters {
+  city: string
+  age: Age | null
+  size: Size | null
+}
 export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   findById(id: string): Promise<Pet | null>
+  findByCity(data: SearchPetsFilters): Promise<Pet[]>
 }
