14d2542df8dd78f21d98782047dcd2ebc4a191b0
luanavfg <luanavefago1@gmail.com>
2024-02-07 08:06:55 -0300

    feat: add method findByCity in prisma pets repository

--- src/repositories/prisma/prisma-pets-repository.ts
+++ src/repositories/prisma/prisma-pets-repository.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
-import { PetsRepository } from '../pets-repository'
+import { PetsRepository, SearchPetsFilters } from '../pets-repository'
 
 export class PrismaPetsRepository implements PetsRepository {
   async create(data: Prisma.PetUncheckedCreateInput) {
@@ -19,5 +19,16 @@ export class PrismaPetsRepository implements PetsRepository {
     })
 
     return pet
+  }
+
+  async findByCity({ city, query }: SearchPetsFilters) {
+    const pets = await prisma.pet.findMany({
+      where: {
+        city,
+        ...query,
+      },
+    })
+
+    return pets
   }
 }
