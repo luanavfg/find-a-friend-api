650ac8084569db2066378f2176904040e08416a3
luanavfg <luanavefago1@gmail.com>
2024-02-07 13:18:14 -0300

    feat: add getSpecificPet route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,11 +4,13 @@ import { authenticate } from './controllers/organizations/authenticate'
 import { createPet } from './controllers/pets/create-pet'
 import { verifyJWT } from './middlewares/verify-jwt'
 import { searchPets } from './controllers/pets/search-pets'
+import { getSpecificPet } from './controllers/pets/get-specific-pet'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/organizations', register)
   app.post('/sessions', authenticate)
 
-  app.post('/pets', { onRequest: [verifyJWT] }, createPet)
   app.get('/pets/:city', searchPets)
+  app.get('/pets/:id', getSpecificPet)
+  app.post('/pets', { onRequest: [verifyJWT] }, createPet)
 }
