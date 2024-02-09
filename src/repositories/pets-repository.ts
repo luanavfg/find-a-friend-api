4a487a27331573f967722f27596a08d609e58158
luanavfg <luanavefago1@gmail.com>
2024-02-09 10:36:55 -0300

    feat: add deleteAll method in repository to be used in tests

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -10,4 +10,5 @@ export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   findById(id: string): Promise<Pet | null>
   findByCity(data: SearchPetsFilters): Promise<Pet[]>
+  deleteAll(): Promise<void>
 }
